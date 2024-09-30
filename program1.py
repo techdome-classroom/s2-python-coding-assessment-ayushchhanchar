063ef6b6e38ccdd6ad01e793c37e2be048ecbd72
ayushchhanchar <chhancharayush007@gmail.com>
2024-09-30 14:27:43 +0530

    ques solves python

--- program1.py
+++ program1.py
@@ -1,20 +1,22 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+       
+        stack = []
+        
+        matching_pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+
+        for char in s:
+            if char in matching_pairs:
+                stack.append(char)
+            else:
+                if not stack or matching_pairs[stack.pop()] != char:
+                    return False
+        return len(stack) == 0
 
 
-
-
-
-
-
-    
-
-
-
-  
+# python -m unittest test1.py
 
--- program2.py
+++ program2.py
@@ -1,10 +1,18 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+    def romanToInt(self, s: str) -> int:
+        # Map of Roman numerals to integer values
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
 
+        total = 0  # Variable to store the result
 
+        # Iterate through the string
+        for i in range(len(s)):
+            # If the current numeral is smaller than the next numeral, subtract its value
+            if i < len(s) - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                total -= roman_to_int[s[i]]
+            else:
+                total += roman_to_int[s[i]]
 
+        return total
